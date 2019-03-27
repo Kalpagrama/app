69317bffb60616fea3a424f4d76f85d9d546fbf0
dev <kalpagrama@gmail.com>
2019-03-27 11:32:39 +0300

    Доработал свайп, скролл и пан ленты

--- src/components/GrainList.vue
+++ src/components/GrainList.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
-    .kp-grain-list(ref="list" v-touch-swipe.up="swipeUp" v-touch-swipe.down="swipeDown")
+    .kp-grain-list(ref="list" v-touch-swipe.up="swipeUp" v-touch-swipe.down="swipeDown"
+    v-touch-pan.up="panUp" v-touch-pan.down="panDown"  @wheel.prevent="catchScroll")
         //.kp-grain-list__container(v-touch-swipe.mouse.up.down="switchGrain")
         .kp-grain-list__container
             grain(flat bordered v-for="(card, ix) in source" :key="ix" :item="{id: card}" :ix="ix" :ref="'card-' + ix")
@@ -33,17 +34,17 @@
                 const ref = this.$refs.list;
                 const rect = this.$refs['card-0'][0].$el.getBoundingClientRect();
                 const mainRect = ref.getBoundingClientRect();
-                console.log('main', mainRect);
 
                 // eslint-disable-next-line
                 let { target, timer } = this;
                 let delta = rect.top > 0 ? 30 : 0; // смещение для позиционирования по центру
                 if (mainRect.height <= rect.height) delta = 0;
+                delta = 0;
 
                 target += Math.sign(by) * (rect.height + MARGIN - delta);
                 this.timer = setInterval(() => {
                     if (ref) {
-                        const newValue = parseInt(target * 2 / 3, 0);
+                        const newValue = parseInt(target * 1 / 2, 0);
                         ref.scrollBy(0, target - newValue);
                         target = newValue;
                     }
@@ -51,13 +52,23 @@
                         clearInterval(timer);
                         timer = 0;
                     }
-                }, 50);
+                }, 10);
             },
             swipeUp() {
-                this.scroll(300);
+                this.scroll(1);
             },
             swipeDown() {
-                this.scroll(-300);
+                this.scroll(-1);
+            },
+            panUp() {
+                if (!this.timer) this.scroll(1);
+            },
+            panDown() {
+                if (!this.timer) this.scroll(-1);
+            },
+            catchScroll(e) {
+                if (e.deltaY > 0) this.scroll(1);
+                else if (e.deltaY < 0) this.scroll(-1);
             },
         },
     };
