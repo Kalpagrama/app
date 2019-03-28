e99630195a442dba5283781d97ac1375b9553b68
dev <kalpagrama@gmail.com>
2019-03-28 10:37:33 +0300

    Добавил каркас компонента Setting.vue

---
+++ src/components/Setting.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    .kp-setting
+        h3 Title
+</template>
+
+<script>
+    export default {
+        name: 'Setting'
+    }
+</script>
+
+<style lang="stylus">
+    .kp-setting
+        display block
+        padding 16px
+        border-top 1px solid silver
+        min-height 200px
+</style>
