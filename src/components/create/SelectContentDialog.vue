563203a99e848fc8128b12041314a40d4b72d367
dev <kalpagrama@gmail.com>
2019-04-18 19:16:19 +0300

    Добавил окно выбора источника контента

--- src/components/create/SelectContentDialog.vue
+++ src/components/create/SelectContentDialog.vue
@@ -1,45 +1,68 @@
 <template lang="pug">
-    q-dialog(v-model='isActive')
+    q-dialog(v-model='isVisible')
         q-card
-            q-card-section.row.items-center
-                .text-h6 Close icon
-                q-space
-                    q-btn(icon='close', flat='', round='', dense='', v-close-popup='')
-            q-card-section Some Text
+            q-card-section
+                .text-h6 Добавление контента
+            q-card-section.sources
+                q-btn.sources__item(v-for="(item,ix) in sources"
+                    :key="ix" @click="open(item)" :icon="item.icon" :label="item.label")
+
+            input(type="file" ref="input" hidden)
+
+            q-card-actions
+                q-btn(flat label="Отмена" color="primary" v-close-popup)
 
 </template>
 
 <script>
+const CODE_YOUTUBE = 'youtube';
+const CODE_IMAGE = 'image';
+
 export default {
     name: 'SelectContentDialog',
     props: {
-        active: {
+        model: {
             type: Boolean,
             default: false,
         },
     },
     data() {
         return {
-            isActive: true,
+            isVisible: false,
+            sources: [
+                { code: CODE_IMAGE, label: 'Выбрать фото', icon: 'photo_library' },
+                { code: CODE_YOUTUBE, label: 'Найти ролик на Youtube', icon: 'ondemand_video' },
+            ],
         };
     },
     watch: {
-        active(value) {
-//            console.log('active', value);
-            this.isActive = value;
+        isVisible(val) {
+            if (!val) this.$emit('close');
         },
-        isActive(value) {
-//            console.log('isActive', value);
-            if (!value) this.$emit('close');
-        },
+        model(val) {
+            this.isVisible = val;
+        }
     },
     computed: {
     },
     methods: {
+        open(item) {
+            if (item.code === CODE_IMAGE) {
+                this.$refs.input.click();
+            } else if (item.code === CODE_YOUTUBE) {
+
+            }
+        }
     },
 };
 </script>
 
 <style lang="stylus">
-
+    .sources
+        &__item
+            display block
+            width 100%
+            margin-bottom 8px
+            &:last-child
+                margin-bottom 0
 </style>
--- src/pages/mobile/Create.vue
+++ src/pages/mobile/Create.vue
@@ -1,20 +1,15 @@
 <template lang="pug">
   q-page.flex
     .kp-create
-        content-creator(@create="showSelectContent")
+        content-creator(@create="showSelectContent(0)")
 
         q-item
             q-item-section
                 span.kp-create__sense-text Текст сути
 
-        content-creator(@create="showSelectContent")
+        content-creator(@create="showSelectContent(1)")
 
-    .test
-        q-btn(@click="gqlRefresh") TEST
-        .test-1(v-for="(item, ix) in services()" :key="ix") {{ item.name }}&nbsp;
-            a(:href="item.url") {{ item.url }}
-
-    //select-content-dialog(:active="true" @close="mode = MODE_NONE")
+    select-content-dialog(:model="selectVisible" @close="closeDialog")
     //select-content-dialog(:active="mode === MODE_SELECT_CONTENT" @close="mode = MODE_NONE")
 </template>
 
@@ -32,10 +27,10 @@
 <script>
 import ContentCreator from '../../components/create/ContentCreator';
 import SelectContentDialog from '../../components/create/SelectContentDialog';
-// import { Auth } from '../../store/auth-old';
 
 const MODE_NONE = 'mode.none';
 const MODE_SELECT_CONTENT = 'mode.select-content';
+const INDEX_NO_CONTENT = -1;
 
 export default {
   name: 'PageMobileCreate',
@@ -43,47 +38,28 @@ export default {
     data() {
       return {
           mode: MODE_NONE,
-          MODE_NONE,
-          MODE_SELECT_CONTENT,
+          selectVisible: false,
           response: {},
+          targetContentIndex: INDEX_NO_CONTENT,
       };
     },
+    watch: {
+      mode(val) {
+          this.selectVisible = (val === MODE_SELECT_CONTENT);
+      }
+    },
     methods: {
-        showSelectContent() {
+        showSelectContent(index) {
           this.mode = MODE_SELECT_CONTENT;
+          this.targetContentIndex = index;
         },
         services() {
             return this.response;
         },
-        gqlRefresh() {
-            const request = {
-                query: gql`query{
-                    response: discoverServices {
-                        name
-                        url
-                    }
-                }`
-            };
-
-            const self = this;
-
-            return new Promise((resolve) => {
-                // const selfie = self;
-                this.$apollo.subscribe(request).subscribe({
-                    next: ({ data: { response } }) => {
-                        self.response = response;
-                        resolve(response);
-                }
-                })
-            });
-        },
-        async test() {
-            /*
-            const auth = new Auth(this);
-            const a = await auth.listServices();
-            return a;
-            */
-        },
+        closeDialog() {
+            this.mode = MODE_NONE;
+            this.targetContentIndex = INDEX_NO_CONTENT;
+        }
     },
 };
 </script>
