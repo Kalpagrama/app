389687fe1ef0bea78e9c30688c8cc10a74399298
4321ip <4321ip@gmail.com>
2019-12-11 19:48:23 +0300

    category add fix

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,7 +1,10 @@
 import assert from 'assert'
 
 import { apolloProvider } from 'boot/apollo'
-
+import { getLogFunc, LogLevelEnum, LogModulesEnum } from 'src/boot/log'
+const logD = getLogFunc(LogLevelEnum.DEBUG, LogModulesEnum.VUEX)
+const logE = getLogFunc(LogLevelEnum.ERROR, LogModulesEnum.VUEX)
+const logW = getLogFunc(LogLevelEnum.WARNING, LogModulesEnum.VUEX)
 // todo переместить текущего пользователя в objects
 export const init = async (context) => {
   if (context.state.initialized) return
@@ -10,5 +13,16 @@ export const init = async (context) => {
 }
 
 export const setFavouriteCategories = async (context, categoryTypes) => {
-
+  // logD('', categoryTypes)
+  let { data: { setFavouriteCategories } } = await apolloProvider.clients.apiApollo.mutate({
+    mutation: gql`
+      mutation setFavouriteCategories ($categories: [CategoryEnum!]!){
+        setFavouriteCategories(categories: $categories)
+      }
+    `,
+    variables: {
+      categories: categoryTypes
+    }
+  })
+  return setFavouriteCategories
 }
