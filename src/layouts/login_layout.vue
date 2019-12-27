7b549f9a7a784a0c19235a68f2d4a2955a92818d
dev <kalpagrama@gmail.com>
2019-12-27 18:17:23 +0300

    login layout

--- src/layouts/login_layout.vue
+++ src/layouts/login_layout.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
-div(
-  :style=`{position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, height: '100%'}`
-  ).column.full-width.bg-primary
-  div(:style=`{height: 400+'px'}`).row.full-width.content-center
-    k-spinner(:width="100" :height="100" @click.native="$go('/login')")
-    .row.full-width.justify-center.q-mt-sm
-      span.text-h6.text-white.text-bold KALPAGRAMMA
-  .col.full-width.scroll
-    router-view
+q-layout().bg-primary
+  q-page-container
+    .column
+      div(:style=`{height: 400+'px'}`).row.full-width.content-center
+        k-spinner(:width="100" :height="100" @click.native="$go('/login')")
+        .row.full-width.justify-center.q-mt-sm
+          span.text-h6.text-white.text-bold KALPAGRAMMA
+      .col.full-width.scroll
+        router-view
 </template>
 
 <script>
