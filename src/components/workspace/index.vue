0ebaa0a24f96a2275a6a08df13fa386ab7b5e971
dev <kalpagrama@gmail.com>
2020-02-11 12:18:39 +0300

    FIX kalpaLoader

--- src/components/workspace/index.vue
+++ src/components/workspace/index.vue
@@ -4,7 +4,7 @@ q-layout(view="hHh lpR fFf")
   q-page-container(
   ).row.full-width.window-height.bg-black
     ws-items(
-      :ctx="$q.screen.gt.sm ? 'workspace' : 'finder'"
+      :ctx="$q.screen.gt.xs ? 'workspace' : 'finder'"
       :oid="node ? node.oid : false" :page="page"
       @page="$router.push({params: {page: $event}})" @item="itemClick").bg-grey-9
     .col.full-height.bg-grey-10
