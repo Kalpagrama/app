adc114f9c240581b091c3128aa0d4e85d5c3019f
4321ip <4321ip@gmail.com>
2020-04-01 15:08:07 +0300

    создание цепочки работает

--- src/store/node/actions.js
+++ src/store/node/actions.js
@@ -107,76 +107,107 @@ export const nodeDelete = async (context, oid) => {
   return deleteObject
 }
 
-export const nodeCreate = async (context, node) => {
-  logD('nodeCreate start', node)
-  // checks
+function makeCompositionInput (composition){
+  assert.ok(composition.layers.length > 0)
+  assert(composition.operation, 'operation')
+  let compositionLen = 0
+  for (let l of composition.layers) {
+    assert.ok(l.content && l.content.oid)
+    assert(l.spheres && l.spheres.length >= 0 && l.spheres.length <= 10)
+    assert.ok(l.figuresAbsolute && l.figuresAbsolute.length === 2)
+    let start = l.figuresAbsolute[0].t
+    let end = l.figuresAbsolute[1].t
+    assert.ok(start >= 0 && end > 0)
+    assert.ok(end > start)
+    compositionLen += (end - start)
+  }
+  assert(compositionLen <= 60)
+  return {
+    thumbUrl: composition.thumbUrl,
+    spheres: composition.spheres ? composition.spheres.map(s => ({ name: s.name })) : [],
+    layers: composition.layers.map(l => {
+      return {
+        contentOid: l.contentOid || l.content.oid,
+        speed: l.speed,
+        figuresAbsolute: l.figuresAbsolute.map(f => {
+          return {
+            t: f.t,
+            points: f.points.map(p => {
+              return { x: p.x, y: p.y }
+            })
+          }
+        }),
+        spheres: l.spheres.map(s => {
+          return { name: s.name }
+        }),
+        color: l.color,
+        thumbUrl: l.thumbUrl
+      }
+    }),
+    operation: {
+      items: composition.operation.items,
+      operations: composition.operation.operations,
+      type: composition.operation.type
+    }
+  }
+}
+function makeNodeInput (node){
   {
+    // checks
     assert.ok(node.category, 'node.category')
     assert.ok(node.spheres.length >= 0 && node.spheres.length <= 10, 'node spheres')
     assert.ok(node.compositions.length > 0, 'node.compositions.length > 0')
     assert.ok(['PIP', 'SLIDER', 'VERTICAL', 'HORIZONTAL'].includes(node.layout), 'node.layout')
-    for (let c of node.compositions) {
-      assert.ok(c.layers.length > 0)
-      // assert(c.spheres && c.spheres.length >= 0 && c.spheres.length <= 10, 'c.spheres')
-      assert(c.operation, 'c.operation')
-      let compositionLen = 0
-      for (let l of c.layers) {
-        assert.ok(l.content && l.content.oid)
-        assert(l.spheres && l.spheres.length >= 0 && l.spheres.length <= 10)
-        assert.ok(l.figuresAbsolute && l.figuresAbsolute.length === 2)
-        let start = l.figuresAbsolute[0].t
-        let end = l.figuresAbsolute[1].t
-        assert.ok(start >= 0 && end > 0)
-        assert.ok(end > start)
-        compositionLen += (end - start)
-      }
-      assert(compositionLen <= 60)
-    }
   }
-
   let nodeInput = {}
   nodeInput.layout = node.layout
   nodeInput.name = node.name
   nodeInput.category = node.category || 'FUN'
   nodeInput.spheres = node.spheres.map(s => {
-    return { name: s.name }
+    return { name: s.name, oid: s.oid }
   })
-  nodeInput.compositions = node.compositions.map(c => {
-    return {
-      thumbUrl: c.thumbUrl,
-      spheres: c.spheres ? c.spheres.map(s => ({ name: s.name })) : [],
-      layers: c.layers.map(l => {
-        return {
-          contentOid: l.contentOid || l.content.oid,
-          speed: l.speed,
-          figuresAbsolute: l.figuresAbsolute.map(f => {
-            return {
-              t: f.t,
-              points: f.points.map(p => {
-                return { x: p.x, y: p.y }
-              })
-            }
-          }),
-          spheres: l.spheres.map(s => {
-            return { name: s.name }
-          }),
-          color: l.color,
-          thumbUrl: l.thumbUrl
-        }
-      }),
-      operation: {
-        items: c.operation.items,
-        operations: c.operation.operations,
-        type: c.operation.type
-      }
-    }
+  nodeInput.compositions = node.compositions.map(composition => {
+    return makeCompositionInput(composition)
   })
+  return nodeInput
+}
+function makeChainInput (chain){
+  {
+    // checks
+    assert.ok(chain.spheres.length >= 0 && chain.spheres.length <= 10, 'chain spheres')
+    assert.ok(chain.links.length > 0, 'chain.links.length > 0')
+  }
+  let chainInput = {}
+  chainInput.name = chain.name
+  chainInput.spheres = chain.spheres.map(s => {
+    return { name: s.name, oid: s.oid }
+  })
+  chainInput.links = chain.links.map(link => {
+    assert(link.leftOid || link.leftSphere || link.leftNode || link.leftComposition, '!link.leftOid')
+    assert(link.rightOid || link.rightSphere || link.rightNode || link.rightComposition, 'link.rightOid')
+    assert(link.type, '!link.type')
+    let resultLink = {name: link.name, type: link.type}
+    if (link.leftOid) resultLink.leftOid = link.leftOid
+    if (link.leftSphere) resultLink.leftSphere = link.leftSphere
+    if (link.leftNode) resultLink.leftNode = makeNodeInput(link.leftNode)
+    if (link.leftComposition) resultLink.leftComposition = makeCompositionInput(link.leftComposition)
+    if (link.rightOid) resultLink.rightOid = link.rightOid
+    if (link.rightSphere) resultLink.rightSphere = link.rightSphere
+    if (link.rightNode) resultLink.rightNode = makeNodeInput(link.rightNode)
+    if (link.rightComposition) resultLink.rightComposition = makeCompositionInput(link.rightComposition)
+    return resultLink
+  })
+  return chainInput
+}
 
+export const nodeCreate = async (context, node) => {
+  logD('nodeCreate start', node)
+  let nodeInput = makeNodeInput(node)
   logD('nodeCreate nodeInput', nodeInput)
   let { data: { nodeCreate: createdNode } } = await apollo.clients.api.mutate({
     mutation: gql`
       ${fragments.objectFullFragment}
-      mutation nodeCreate ($node: NodeInput!) {
+      mutation ($node: NodeInput!) {
         nodeCreate (node: $node){
           ...objectFullFragment
         }
@@ -188,12 +219,30 @@ export const nodeCreate = async (context, node) => {
   })
   context.dispatch('cache/update', { key: createdNode.oid, newValue: createdNode, actualAge: 'zero' }, { root: true })
   await context.dispatch('workspace/exportLayersFromNode', node, { root: true }) // сохраним слои из созданного ядра в мастерской
-  logD('nodeCreate done', nodeCreate)
-  return nodeCreate
+  logD('nodeCreate done', createdNode)
+  return createdNode
 }
 
-export const chainCreate = async (context, chainInput) => {
-// todo
+export const chainCreate = async (context, chain) => {
+  logD('chainCreate start', chain)
+  let chainInput = makeChainInput(chain)
+  logD('chainCreate chainInput', chainInput)
+  let { data: { chainCreate: createdChain } } = await apollo.clients.api.mutate({
+    mutation: gql`
+      ${fragments.objectFullFragment}
+      mutation ($chain: ChainInput!) {
+        chainCreate (chain: $chain){
+          ...objectFullFragment
+        }
+      }
+    `,
+    variables: {
+      chain: chainInput
+    }
+  })
+  context.dispatch('cache/update', { key: createdChain.oid, newValue: createdChain, actualAge: 'zero' }, { root: true })
+  logD('chainCreate done', createdChain)
+  return createdChain
 }
 export const rate = async (context, { oid, rateUser }) => {
   // todo
