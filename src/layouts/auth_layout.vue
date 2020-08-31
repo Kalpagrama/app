926b40047cc494a9c17f4f338dc03e7794d48c39
dev <kalpagrama@gmail.com>
2020-08-31 14:11:00 +0300

    FIX withSocials

--- src/layouts/auth_layout.vue
+++ src/layouts/auth_layout.vue
@@ -2,7 +2,9 @@
 <template lang="pug">
 q-layout(view="hHh lpR fFf").b-30
   q-page-container
-    div(:style=`{height: '200px', overflow: 'hidden',}`).row.full-width.items-center.content-center.justify-center.b-30
+    div(
+      @click="$router.replace('/auth')"
+      :style=`{height: '200px', overflow: 'hidden',}`).row.full-width.items-center.content-center.justify-center.b-30
       kalpa-logo(:width="100" :height="100").q-mb-md
       h5.text-white.text-bold.q-ma-xs.q-pa-xs Kalpagramma
     router-view
@@ -16,6 +18,32 @@ export default {
   },
   data () {
     return {
+    }
+  },
+  watch: {
+    '$route.query.token': {
+      immediate: true,
+      async handler (to, from) {
+        this.$log('$route.query.token CHANGED', to)
+        if (to) {
+          this.$log('GOT TOKEN', to)
+          // this.$q.notify('GOT TOKEN' + JSON.stringify(this.$route.query))
+          let q = this.$route.query
+          this.$log('q=', q)
+          localStorage.setItem('k_token', q.token)
+          localStorage.setItem('ktokenExpires', q.expires)
+          // await this.$wait(200)
+          // this.userIdentifying = false
+          // this.userIdentified = true
+          // this.login = q.userId
+          // this.loginType = q.loginType
+          // this.userExist = q.userExist === 'true' ? true : false
+          // this.needInvite = q.needInvite === 'true' ? true : false
+          // this.$log('this.needInvite = ', this.needInvite)
+          // if userExist and !needInvite... this.userAuthenticate()
+          // this.$router.replace('/auth')
+        }
+      }
     }
   },
   mounted () {
