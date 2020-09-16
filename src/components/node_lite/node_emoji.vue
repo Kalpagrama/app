37b4b9d9e28a98deeba87ab2239065785a7cb88e
dev <kalpagrama@gmail.com>
2020-09-16 18:03:27 +0300

    FIX nodeEmoji

--- src/components/node_lite/node_emoji.vue
+++ src/components/node_lite/node_emoji.vue
@@ -1,19 +1,28 @@
 <template lang="pug">
 insert-emoji(
-  @click="start()"
+  @click.native="start()"
   color="grey-8")
 </template>
 
 <script>
+import { RxCollectionEnum } from 'src/system/rxdb'
+
 import insertEmoji from 'components/kalpa_icons/insert_emoji.vue'
 
 export default {
   name: 'nodeEmoji',
   components: {insertEmoji},
   props: ['node'],
-  metods: {
-    start () {
+  data () {
+    return {
+      emojiSpheres: []
+    }
+  },
+  methods: {
+    async start () {
       this.$log('start')
+      this.emojiSpheres = await this.$rxdb.get(RxCollectionEnum.GQL_QUERY, 'emojiSpheres')
+      this.$log('emojiSpheres', this.emojiSpheres)
     }
   }
 }
