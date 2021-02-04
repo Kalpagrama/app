adb021f92eb7e866f313dd574b830471d5c27e1d
dev <kalpagrama@gmail.com>
2021-02-04 16:45:55 +0300

    FIX notifications

--- src/pages/app/notifications/index.vue
+++ src/pages/app/notifications/index.vue
@@ -10,31 +10,39 @@ q-layout(view="hHh Lpr lff")
           ).row.full-width.items-center.content-center.justify-between.q-pa-sm.b-40
           q-icon(name="notifications_none" color="white" size="30px").q-mx-sm
           .col
-            span(
-              :style=`{fontSize: '18px', userSelect: 'none'}`
-              ).text-bold.text-white {{$t('pageApp_notifications_title', 'Уведомления')}}
+          span(
+            :style=`{fontSize: '18px', userSelect: 'none'}`
+            ).text-bold.text-white {{$t('pageApp_notifications_title', 'Уведомления')}}
+          .col
           q-btn(round flat color="grey-8" icon="more_vert")
   q-page-container
-    q-page
+    q-page.row.full-width.justify-center.q-pt-sm.q-px-sm
       list-feed(
+        v-if="sphereOid"
         :query="query"
         :itemStyles=`{
-          paddingBottom: '50px',
+          paddingBottom: '8px',
+        }`
+        :style=`{
+          maxWidth: $store.state.ui.pageWidth+'px',
         }`)
-    //- router-view
-    //- iframe(
-      src="https://doc.clickup.com/p/h/2cve9-15/42d6a6e8f899a3b"
-      frameBorder="0"
-      :style=`{
-        width: '100%', height: 'calc(100vh - 0px)',
-      }`)
+        template(v-slot:item=`{item,itemIndex,isActive,isVisible}`)
+          notification-item(
+            :notification="item"
+            :notificationIndex="itemIndex"
+            :isActive="isActive"
+            :isVisible="isVisible")
 </template>
 
 <script>
 import { RxCollectionEnum } from 'src/system/rxdb'
+import notificationItem from './notification_item.vue'
 
 export default {
   name: 'pageApp__notifications',
+  components: {
+    notificationItem,
+  },
   data () {
     return {
     }
---
+++ src/pages/app/notifications/notification_item.vue
@@ -0,0 +1,79 @@
+<style lang="sass">
+.notification-item
+  &:hover
+    background: rgb(42,42,42) !important
+</style>
+
+<template lang="pug">
+.row.full-width
+  div(
+    :style=`{
+      background: 'rgb(35,35,35)',
+      borderRadius: '10px',
+    }`
+    ).row.full-width
+    .row.full-width.items-center.content-center.q-pa-sm
+      router-link(
+        :to="itemLink(notification.card.items[0])"
+        :style=`{
+          background: 'rgb(38,38,38)',
+          borderRadius: '10px',
+        }`
+        ).row.items-center.content-center.q-pa-sm.notification-item
+        img(
+          draggable="false"
+          :src="notification.card.items[0].thumbUrl"
+          :style=`{
+            height: '30px',
+            borderRadius: '50%',
+          }`)
+        .col.q-px-sm
+          span.text-grey-5.q-mr-sm {{ notification.card.items[0].name }}
+      .col
+        .row.full-width.q-px-sm.justify-end
+          small.text-grey-8 {{ $date(notification.createdAt) }}
+    .row.full-width.q-px-md.q-py-xs
+      span(:style=`{textTransform: 'capitalize',}`).text-white {{ notification.card.items[1] }}
+    .row.full-width.q-px-sm.q-pa-sm
+      router-link(
+        :to="itemLink(notification.card.items[2])"
+        :style=`{
+          background: 'rgb(38,38,38)',
+          borderRadius: '10px',
+        }`
+        ).row.items-center.content-center.q-pa-sm.notification-item
+        img(
+          draggable="false"
+          :src="notification.card.items[2].thumbUrl"
+          :style=`{
+            height: '30px',
+            minWidth: '30px',
+            maxWidth: '70px',
+            borderRadius: '10px',
+          }`)
+        .col.q-px-sm
+          span.text-grey-5 {{ notification.card.items[2].name }}
+</template>
+
+<script>
+export default {
+  name: 'notificationItem',
+  props: ['notification', 'notificationIndex'],
+  methods: {
+    itemLink (item) {
+      let itemMap = {
+        NODE: '/node/',
+        USER: '/user/',
+        WORD: '/sphere/',
+        SENTENCE: '/sphere/',
+        JOINT: '/joint/',
+        VIDEO: '/content/',
+        IMAGE: '/content/',
+        BOOK: '/content/',
+        WEB: '/content/',
+      }
+      return itemMap[item.type] + item.oid
+    }
+  }
+}
+</script>
