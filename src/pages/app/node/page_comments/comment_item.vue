c50a9d99c7d8445dfa1c3658079a98306bdaca43
dev <kalpagrama@gmail.com>
2021-04-06 18:31:57 +0300

    FIX commentItem

--- src/pages/app/node/page_comments/comment_item.vue
+++ src/pages/app/node/page_comments/comment_item.vue
@@ -1,13 +1,46 @@
+<style lang="sass">
+a
+  text-decoration: none !important
+  color: white !important
+  &:hover
+    color: rgb(76,175,80) !important
+</style>
+
 <template lang="pug">
-.row.full-width.items-start.content-start.br
-  div()
+.row.full-width.items-start.content-start
+  //- author.thumbUrl
+  div(
+    :style=`{
+      width: '60px',
+      height: '60px',
+    }`
+    ).row.items-center.content-center.justify-center
+    img(
+      draggable="false"
+      :src="comment.author.thumbUrl"
+      :style=`{
+        width: '30px', minWidth: '30px', maxWidth: '30px',
+        height: '30px', minHeight: '30px', maxHeight: '30px',
+        borderRadius: '50%',
+      }`)
   .col
-    .row.full-width
+    .row.full-width.q-pt-sm
       //- author and timestamp
-      .row.full-width
+      .row.full-width.items-end.content-end
+        span(:style=`{maxWidth: '200px', overflow: 'hidden',}`).text-green.q-mr-sm {{ comment.author.name }}
+        small.text-grey-8 {{ $date(comment.createdAt) }}
       //- body
       .row.full-width
-        small.text-white {{ comment }}
+        p(
+          v-if="text"
+          v-html="text"
+          :style=`{
+            whiteSpace: 'pre-wrap',
+            color: 'white !important',
+            fontSize: '14px',
+            textDecotation: 'none !important',
+          }`
+          ).text-white
       //- TODO: actions
       .row.full-width
 </template>
@@ -18,7 +51,28 @@ export default {
   props: ['comment'],
   data () {
     return {
+      text: null,
     }
+  },
+  computed: {
+    words () {
+      return this.comment.text.split(' ')
+    }
+  },
+  methods: {
+    urlify (text) {
+      var urlRegex = /(https?:\/\/[^\s]+)/g;
+      return text.replace(urlRegex, function(url) {
+        return '<a target="_blank" href="' + url + '">' + url + '</a>';
+      })
+      // or alternatively
+      // return text.replace(urlRegex, '<a href="$1">$1</a>')
+    },
+  },
+  mounted () {
+    this.$log('mounted')
+    var text = 'Find me at http://www.example.com and also at http://stackoverflow.com';
+    this.text = this.urlify(this.comment.text)
   }
 }
 </script>
