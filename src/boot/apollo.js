ee2af5cf10057f73cbd6f3c6e2bd6fea66e197ff
4321ip <4321ip@gmail.com>
2021-08-06 22:46:02 +0300

    test vercel3

--- src/boot/apollo.js
+++ src/boot/apollo.js
@@ -32,7 +32,10 @@ let apollo
 export default async ({ Vue, store, app }) => {
    try {
       const f = { nameExtra: 'boot::apollo' }
-      logW(f, 'start', 'process.env=', process.env)
+      logW(f, 'start', 'process.env=', JSON.stringify(process.env), process.env.SERVICES_URL_DEBUG, process.env.DOCKER_MACHINE_NAME, process.env.VERCEL_BUILD)
+      logW(f, 'SERVICES_URL_DEBUG=', process.env.SERVICES_URL_DEBUG)
+      logW(f, 'DOCKER_MACHINE_NAME=', process.env.DOCKER_MACHINE_NAME)
+      logW(f, 'VERCEL_BUILD=', process.env.VERCEL_BUILD)
       const t1 = performance.now()
       let fetchFunc
       // console.error('apollo::isSsr=', isSsr)
