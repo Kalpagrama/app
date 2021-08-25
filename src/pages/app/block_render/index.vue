ef38989db50b2700170f9231ab6d741a3f6f1e33
4321ip <4321ip@gmail.com>
2021-08-25 09:04:47 +0300

    v3.2.315

---
+++ src/pages/app/block_render/index.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+div(
+  v-if="node"
+  ).row.full-width.justify-center
+  item-feed(
+    :item="node"
+    :isActive="false"
+    :isVisible="false"
+    :showAuthorAlways="true"
+    :showActions="false")
+</template>
+
+<script>
+import { RxCollectionEnum } from 'src/system/rxdb'
+
+export default {
+  name: 'pageAppNodeRender',
+  data () {
+    return {
+      node: null,
+    }
+  },
+  watch: {
+    '$route.params.oid': {
+      deep: true,
+      immediate: true,
+      async handler (to, from) {
+        if (to) {
+          // this.$log('$route.params.oid TO', to)
+          this.node = await this.$rxdb.get(RxCollectionEnum.OBJ, to)
+        }
+      }
+    },
+  },
+  created () {
+    this.$store.commit('ui/stateSet', ['mobileNavigationShow', false])
+    this.$store.commit('ui/stateSet', ['desktopNavigationShow', false])
+  },
+  beforeDestroy () {
+    this.$store.commit('ui/stateSet', ['mobileNavigationShow', true])
+    this.$store.commit('ui/stateSet', ['desktopNavigationShow', true])
+  }
+}
+</script>
