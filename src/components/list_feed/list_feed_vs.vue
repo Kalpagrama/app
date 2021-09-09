ea635c71f6339bc2d4a2e9a43744c8cd997339e0
4321ip <4321ip@gmail.com>
2021-09-09 09:46:15 +0300

    WIP

--- src/components/list_feed/list_feed_vs.vue
+++ src/components/list_feed/list_feed_vs.vue
@@ -3,19 +3,16 @@
     .row.full-width
       q-spinner-dots(v-if="!itemsRes" color="green" size="60px").absolute-center
       //- items
-      div(
-        v-if="itemsRes"
-        :style=`{ position: 'relative'}`
-      ).row.full-width.items-start.content-start.br
+      div( v-if="itemsRes" :style=`{ position: 'relative'}`).row.full-width.items-start.content-start.br
         q-scroll-area(
           :style=`{maxHeight: scrollAreaHeight+'px', height: scrollAreaHeight+'px'}`
           dark
           :thumb-style=`{
-            right: '5px',
-            borderRadius: '8px',
-            backgroundColor: 'rgb(50,50,50)',
-            width: '8px',
-            opacity: 0.75}`
+          right: '5px',
+          borderRadius: '8px',
+          backgroundColor: 'rgb(50,50,50)',
+          width: '8px',
+          opacity: 0.75}`
           :bar-style=`{
           right: '2px',
           borderRadius: '14px',
@@ -30,26 +27,29 @@
         ).full-width
           q-virtual-scroll(
             scroll-target="#scroll-area-with-virtual-scroll-1 > .scroll"
+            dark
             :items-fn="itemsFn"
             :items-size="length"
             :virtual-scroll-item-size="itemHeightApprox"
-            virtual-scroll-slice-ratio-before="0.5"
-            virtual-scroll-slice-ratio-after="0.5"
-            separator)
+            @virtual-scroll="onScroll")
             template(v-slot:default=`{ item, index }`)
-              q-item(
-                :key="index"
-                dense
+              q-item( :key="index" dense
+              :accessKey="`${item[itemKey]}-${index}`"
+              v-observe-visibility=`{
+                throttle: 300,
+                callback: itemVisibilityHandler,
+                intersection: {
+                   root: scrollTargetIsWindow ? null : scrollTarget,
+                   threshold: 0.2,
+                },
+                }`
               )
-                q-item-section
-                  q-item-label
-                    //#{{ index }} - {{ item.name }}
-                    slot(
-                      name="item"
-                      :item="item"
-                      :itemIndex="index"
-                      :isActive="false"
-                      :isVisible="false")
+                slot(
+                  name="item"
+                  :item="item"
+                  :itemIndex="index"
+                  :isActive="false"
+                  :isVisible="!!itemsVisibility[item[itemKey]]")
 </template>
 
 <script>
@@ -65,7 +65,7 @@ export default {
   props: {
     scrollAreaHeight: { // если не указано - то скролл - весь window (иначе скролл занимает отведенное место)
       type: Number,
-      default: this.$q.screen.height
+      default: 300
     },
     query: {
       type: Object,
@@ -80,13 +80,18 @@ export default {
   data () {
     return {
       itemsRes: null,
-      nonReactiveItems: []
+      nonReactiveItems: [],
+      itemsVisibility: {},
+      itemMiddleIndx: null,
     }
   },
   computed: {
     length () {
       return this.itemsRes ? this.itemsRes.items.length : 0
-    }
+    },
+    itemKey () {
+      return this.itemsRes?.itemPrimaryKey
+    },
   },
   watch: {
     query: {
@@ -100,12 +105,23 @@ export default {
     async gotoPercent (percent) {
     },
     itemsFn (from, size) {
+      this.$log('itemsFn:', from, size, this.itemsRes.items.length)
       assert(this.itemsRes && from < this.itemsRes.items.length && from + size <= this.itemsRes.items.length)
       let result = Object.freeze(cloneDeep((this.itemsRes.items.slice(from, from + size))))
       assert(result.length === size)
       this.$log('result=', result)
       return result
+    },
+    itemVisibilityHandler (isVisible, entry) {
+      let [key, idxSting] = entry.target.accessKey.split('-')
+      if (isVisible) this.$log('itemVisibilityChanged', isVisible, idxSting, key)
+      this.$set(this.itemsVisibility, key, isVisible)
+    },
+    onScroll(details) {
+      this.itemMiddleIndx = Math.floor((details.from + details.to) / 2)
+      this.$log('scroll', this.itemMiddleIndx, details)
     }
+
   },
   mounted () {
     this.$log('mounted', this.scrollAreaHeight, this.$q.screen.height)
