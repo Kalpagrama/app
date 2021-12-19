33ee796b8bd4acd74e2974e488347138f079dfcc
4321ip <4321ip@gmail.com>
2021-12-19 21:10:09 +0300

    update packets

--- src/system/rxdb/index.js
+++ src/system/rxdb/index.js
@@ -142,6 +142,7 @@ class RxDBWrapper {
                      //    await this.destroy('destroy') // это не надо приноральной работе rxdb
                      //    await this.create(this.store) // это не надо при норальной работе rxdb
                      // }
+                     // alert('k_rxdb_reset_date')
                      await systemInit()
                      break
                   case 'k_rxdb_set_auth_user':
@@ -153,6 +154,7 @@ class RxDBWrapper {
                      //    await this.destroy('destroy') // это не надо приноральной работе rxdb
                      //    await this.create(this.store) // это не надо при норальной работе rxdb
                      // }
+                     // alert('k_rxdb_set_auth_user')
                      await systemInit()
                      break
                   default:
