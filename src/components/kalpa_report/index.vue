ccf669f5cc72973ca15c71661cd36c504366a800
Andrey Ivanov <xaxaxx@bk.ru>
2021-12-21 17:07:44 +0300

    kalpa-report cosmetic fix

--- src/components/kalpa_report/index.vue
+++ src/components/kalpa_report/index.vue
@@ -6,10 +6,30 @@ borderRadius: '20px'
   .column.full-width.q-py-md.q-px-sm.q-gutter-y-sm.text-white.items-center
     .row.full-width.justify-center.text-white.q-mb-sm.q-pl-lg.q-pr-sm
       //q-icon(name="report" size="35px" color="red")
-      span(:style=`{fontSize: '16px'}`).text-white.text-center.text-bold {{$t('Report')}}
+      span(:style=`{fontSize: '15px'}`).text-white.text-center {{$t('Report')}}
       q-btn(round flat color="white" icon="clear" v-close-popup).absolute-top-right
-    q-select(v-model="model" dark behavior="dialog" :options="options" outlined color='green' :label="$t('Report reason')" label-color="white" stack-label bg-color='grey-9' options-cover :style=`{width: '300px'}`)
-    q-input(v-model="text" outlined :style=`{width: '300px'}` :placeholder="$t('Message..')" color='green' autogrow :input-style="{ color: '#fff' }" bg-color='grey-9')
+    q-select(
+      v-model="model"
+      dark
+      :options="options"
+      outlined color='green'
+      :label="$t('Report reason')"
+      label-color="white"
+      stack-label
+      bg-color='grey-9'
+      options-cover
+      :style=`{width: '300px'}`
+      )
+    q-input(
+      v-model="text"
+      outlined
+      :style=`{width: '300px'}`
+      :placeholder="$t('Message..')"
+      color='green'
+      autogrow
+      :input-style="{ color: '#fff' }"
+      bg-color='grey-9'
+      )
     .row.full-width.justify-center.q-gutter-sm
       //q-btn(:label= "$t('Cancel')" no-caps outline color="green" v-close-popup)
       q-btn(:label= "$t('Send Report')" no-caps outline color="red" @click="hide")
