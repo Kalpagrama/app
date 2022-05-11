844089b0adff06a8ab53d057284d381bea17aa49
igor <4321ip@mail.ru>
2022-05-11 21:26:30 +0300

    fix bug on show actions

--- src/components/kalpa_item/item_extended/node_extended/index.vue
+++ src/components/kalpa_item/item_extended/node_extended/index.vue
@@ -11,7 +11,7 @@
         .col
         //- tutorial
         kalpa-menu-actions(
-          v-if="showDropdown"
+          v-if="showDropdown && node"
           icon="more_vert"
           color="grey-8"
           :actions="actions")
